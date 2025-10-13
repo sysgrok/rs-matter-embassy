e0a1cc10f237d9f5da45e6ab1c3ef850e8eea437
ivmarkov <ivan.markov@gmail.com>
2025-10-13 22:16:27 +0300

    Update to embassy-nrf 0.8; update to latest embassy-rp; update to latest esp-hal (#21)

--- examples/nrf/src/bin/light_eth.rs
+++ examples/nrf/src/bin/light_eth.rs
@@ -65,7 +65,7 @@ macro_rules! mk_static {
     ($t:ty,$val:expr) => {{
         static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
         #[deny(unused_attributes)]
-        let x = STATIC_CELL.uninit().write(($val));
+        let x = STATIC_CELL.uninit().write($val);
         x
     }};
 }
@@ -286,9 +286,6 @@ const NODE: Node = Node {
 };
 
 #[embassy_executor::task]
-async fn run_radio(
-    mut runner: PhyRadioRunner<'static>,
-    radio: NrfRadio<'static, embassy_nrf::peripherals::RADIO>,
-) -> ! {
+async fn run_radio(mut runner: PhyRadioRunner<'static>, radio: NrfRadio<'static>) -> ! {
     runner.run(radio, EmbassyTimeTimer).await
 }
